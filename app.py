8e37c36c7e34281fbc1c2dc1ef97316bff8f7767
Michael Schwarz <mschwarz137@gmail.com>
2017-03-23 19:25:57 -0400

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   app.py
    	modified:   templates/index.html

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request, redirect
+import quandl as qd
+from bokeh.plotting import figure,show
+from bokeh.resources import CDN
+from bokeh.embed import file_html
 
 app = Flask(__name__)
 
@@ -10,5 +14,21 @@ def main():
 def index():
   return render_template('index.html')
 
+@app.route('/view_ticker', methods=['GET', 'POST'])
+def view_ticker():
+
+    #problem line:
+  d = qd.get("WIKI/" + request.form['ticker'] + ".4", rows = 20)
+
+  f = figure(x_axis_type='datetime',title = "Closing Price for " + request.form['ticker'] + " for the Past 30 Days")
+  f.line('Date', 'Close', source=d)
+  f.xaxis.axis_label = "Date"
+  f.yaxis.axis_label = "Price"
+  
+
+  html = file_html(f, CDN, "my plot")
+  
+  return html
+
 if __name__ == '__main__':
-  app.run(port=33507)
+  app.run()
